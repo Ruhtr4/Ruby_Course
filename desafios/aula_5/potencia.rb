5fa8ffa767cd2a9074e916469285f141efa11060
Arthur L <arthurpeixinho@gmail.com>
2023-01-24 18:37:27 -0300

    upt potencia.rb

--- desafios/aula_5/potencia.rb
+++ desafios/aula_5/potencia.rb
@@ -1,7 +1,4 @@
-resultado = ''
-
 loop do
-  puts resultado
   puts 'Bem-Vindo a nossa calculadora de expoente'
   puts 'escolha uma das opções abaixo'
   puts '1 - para começar'
@@ -9,19 +6,19 @@ loop do
   print "Opção:"
 
   option = gets.chomp.to_i
-  base = ''
-  expoente = ''
   if option == 1
     def potencia
       puts 'digite o seu valor base:'
       base = gets.chomp.to_i
       puts 'digite o valor do expoente:'
       expoente = gets.chomp.to_i
-    puts "aqui está o seu resultado: #{base ** expoente}"
+    print "aqui está o seu resultado: #{base ** expoente}"
+    ojk = gets.chomp
     end
 
     potencia
-  elsif option == 0
+  else
     break
   end
+  system "clear"
 end
